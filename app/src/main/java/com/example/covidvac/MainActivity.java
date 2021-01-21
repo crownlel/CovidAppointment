29b67f8e3b7c839e37f66a89f1487bd7e5068763
Spyros Mylonas <smylonas@atlabit.com>
2021-01-21 23:08:49 +0200

    MainActivity language functionality

--- app/src/main/java/com/example/covidvac/MainActivity.java
+++ app/src/main/java/com/example/covidvac/MainActivity.java
@@ -5,17 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //LocaleManager.onCreate(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.btnEnter);
         button.setOnClickListener(v -> {
-            
+
+        });
+
+        ImageButton greekBtn = findViewById(R.id.ibGreek);
+        greekBtn.setOnClickListener(v ->{
+            LocaleManager.setLocale(this,"el");
+            this.finish();
+            overridePendingTransition(0, 0);
+            this.startActivity(getIntent());
+            overridePendingTransition(0, 0);
+        });
+        ImageButton engBtn = findViewById(R.id.ibEnglish);
+        engBtn.setOnClickListener(v ->{
+            LocaleManager.setLocale(this,"en");
+            this.finish();
+            overridePendingTransition(0, 0);
+            this.startActivity(getIntent());
+            overridePendingTransition(0, 0);
         });
     }
 }
