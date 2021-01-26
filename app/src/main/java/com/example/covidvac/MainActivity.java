e80c57c9ade1a1ebb8dcb01fd2e1730ccad4711e
gio <george.nickas.gio@gmail.com>
2021-01-26 21:22:29 +0200

    Add firebase to project

--- app/src/main/java/com/example/covidvac/MainActivity.java
+++ app/src/main/java/com/example/covidvac/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //LocaleManager.onCreate(this);
+        LocaleManager.onCreate(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
